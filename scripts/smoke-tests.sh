62f180f86462278b93a6c1a850647d05f8d76061
Kenny Gilfilen <kgilfilen@yahoo.com>
2026-07-27 17:55:13 -0600

    Add live URL job input support

---
+++ scripts/smoke-tests.sh
@@ -0,0 +1,11 @@
+python3 -m pytest tests/unit -v
+
+python3 src/main.py --help
+
+python3 src/main.py --file examples/jobs/sdet_topstep.txt
+
+python3 src/main.py --url https://www.virtualvocations.com/job/senior-qa-engineer-3129270-i.html
+
+python3 src/main.py --examples
+
+git status
